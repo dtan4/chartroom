9aeb7b6b3aa88e3345ca493c2c0433836b0144dc
Daisuke FUJITA <dtanshi45@gmail.com>
2014-11-02 17:05:01 +0900

    Add semicolon

--- spec/chartroom/image_spec.rb
+++ spec/chartroom/image_spec.rb
@@ -3,18 +3,18 @@ require "spec_helper"
 module Chartroom
   describe Image do
     describe "#generate_tree_diagram" do
-      let(:image_1) { double(diagram_description: "1a[label=\"1a\"]") }
-      let(:image_2) { double(diagram_description: "2b[label=\"dtan4/hoge:latest\"]") }
-      let(:image_3) { double(diagram_description: "3c[label=\"dtan4/fuga:latest\"]\n3c -> 2b") }
+      let(:image_1) { double(diagram_description: "1a[label=\"1a\"];") }
+      let(:image_2) { double(diagram_description: "2b[label=\"dtan4/hoge:latest\"];") }
+      let(:image_3) { double(diagram_description: "3c[label=\"dtan4/fuga:latest\"];\n3c -> 2b;") }
       let(:images)  { [image_1, image_2, image_3] }
 
       it "should generate tree diagram in dot language" do
         expect(described_class.generate_tree_diagram(images)).to eq <<-EXPECT
 digraph images {
-1a[label="1a"]
-2b[label="dtan4/hoge:latest"]
-3c[label="dtan4/fuga:latest"]
-3c -> 2b
+1a[label="1a"];
+2b[label="dtan4/hoge:latest"];
+3c[label="dtan4/fuga:latest"];
+3c -> 2b;
 }
         EXPECT
       end
@@ -30,8 +30,8 @@ digraph images {
 
         it "should return diagram description" do
           expect(image.diagram_description).to eq <<-EXPECT
-3c[label="dtan4/fuga:latest"]
-3c -> 2b
+3c[label="dtan4/fuga:latest"];
+3c -> 2b;
         EXPECT
         end
       end
@@ -43,8 +43,21 @@ digraph images {
 
         it "should return diagram description" do
           expect(image.diagram_description).to eq <<-EXPECT
-3c[label="3c"]
-3c -> 2b
+3c[label="3c"];
+3c -> 2b;
+        EXPECT
+        end
+      end
+
+      context "when container has no parent" do
+        let(:info) do
+          {"id" => "3c", "ParentId" => "", "RepoTags" => ["<none>:<none>"]}
+        end
+
+        it "should return diagram description" do
+          expect(image.diagram_description).to eq <<-EXPECT
+3c[label="3c"];
+
         EXPECT
         end
       end
