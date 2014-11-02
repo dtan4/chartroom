971b96d3835f6818f696b792e2de40f2fa9478c6
Daisuke FUJITA <dtanshi45@gmail.com>
2014-11-02 17:01:02 +0900

    Fix test

--- spec/chartroom/image_spec.rb
+++ spec/chartroom/image_spec.rb
@@ -4,16 +4,16 @@ module Chartroom
   describe Image do
     describe "#generate_tree_diagram" do
       let(:image_1) { double(diagram_description: "1a[label=\"1a\"]") }
-      let(:image_2) { double(diagram_description: "2b[label=\"dtan4/hoge\"]") }
-      let(:image_3) { double(diagram_description: "3c[label=\"dtan4/fuga\"]\n3c -> 2b") }
+      let(:image_2) { double(diagram_description: "2b[label=\"dtan4/hoge:latest\"]") }
+      let(:image_3) { double(diagram_description: "3c[label=\"dtan4/fuga:latest\"]\n3c -> 2b") }
       let(:images)  { [image_1, image_2, image_3] }
 
       it "should generate tree diagram in dot language" do
         expect(described_class.generate_tree_diagram(images)).to eq <<-EXPECT
 digraph images {
 1a[label="1a"]
-2b[label="dtan4/hoge"]
-3c[label="dtan4/fuga"]
+2b[label="dtan4/hoge:latest"]
+3c[label="dtan4/fuga:latest"]
 3c -> 2b
 }
         EXPECT
@@ -21,17 +21,32 @@ digraph images {
     end
 
     describe "#diagram_description" do
-      let(:info) do
-        {"id" => "3c", "ParentId" => "2b", "RepoTags" => ["dtan4/fuga"]}
-      end
-
       let(:image) { described_class.new(double(info: info)) }
 
-      it "should return diagram description" do
-        expect(image.diagram_description).to eq <<-EXPECT
-3c[label="dtan4/fuga"]
+      context "when container is tagged" do
+        let(:info) do
+          {"id" => "3c", "ParentId" => "2b", "RepoTags" => ["dtan4/fuga:latest"]}
+        end
+
+        it "should return diagram description" do
+          expect(image.diagram_description).to eq <<-EXPECT
+3c[label="dtan4/fuga:latest"]
 3c -> 2b
         EXPECT
+        end
+      end
+
+      context "when container is untagged" do
+        let(:info) do
+          {"id" => "3c", "ParentId" => "2b", "RepoTags" => ["<none>:<none>"]}
+        end
+
+        it "should return diagram description" do
+          expect(image.diagram_description).to eq <<-EXPECT
+3c[label="3c"]
+3c -> 2b
+        EXPECT
+        end
       end
     end
   end
