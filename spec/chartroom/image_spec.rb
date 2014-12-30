1e555aa0ee50270c72d77069e3f5fb3e58553a3e
Daisuke Fujita <dtanshi45@gmail.com>
2014-12-30 19:32:58 +0900

    Add tests for Image methods

--- spec/chartroom/image_spec.rb
+++ spec/chartroom/image_spec.rb
@@ -40,27 +40,117 @@ image_3c -> image_2b;
       end
     end
 
+    describe "#id" do
+      let(:image) { described_class.new(double(info: info)) }
+      let(:info)  { { "id" => id } }
+      let(:id)    { "12a324d4afc11e971cb86467681c0c94ff5bc0e946055ff92526bebee9477216" }
+
+      it "should return id parameter" do
+        expect(image.id).to eq id
+      end
+    end
+
+    describe "#short_id" do
+      let(:image) { described_class.new(double(info: info)) }
+      let(:info)  { { "id" => id } }
+      let(:id)    { "12a324d4afc11e971cb86467681c0c94ff5bc0e946055ff92526bebee9477216" }
+
+      it "should return shortened id" do
+        expect(image.short_id).to eq id[0..11]
+      end
+    end
+
+    describe "#repo_tags" do
+      let(:image)     { described_class.new(double(info: info)) }
+      let(:info)      { { "RepoTags" => repo_tags } }
+      let(:repo_tags) { ["hoge/fuga:latest"] }
+
+      it "should return repo_tags parameter" do
+        expect(image.repo_tags).to eq repo_tags
+      end
+    end
+
+    describe "#virtual_size" do
+      let(:image)        { described_class.new(double(info: info)) }
+      let(:info)         { { "VirtualSize" => virtual_size } }
+      let(:virtual_size) { 12345 }
+
+      it "should return virtual_size parameter" do
+        expect(image.virtual_size).to eq virtual_size
+      end
+    end
+
+    describe "#created_at" do
+      let(:image)      { described_class.new(double(info: info)) }
+      let(:info)       { { "Created" => created_at } }
+      let(:created_at) { 14193560445 }
+
+      it "should return created_at parameter" do
+        expect(image.created_at).to eq created_at
+      end
+    end
+
+    describe "#parent_id" do
+      let(:image)     { described_class.new(double(info: info)) }
+      let(:info)      { { "ParentId" => parent_id } }
+      let(:parent_id) { "12a324d4afc11e971cb86467681c0c94ff5bc0e946055ff92526bebee9477216" }
+
+      it "should return parent_id parameter" do
+        expect(image.parent_id).to eq parent_id
+      end
+    end
+
+    describe "#short_parent_id" do
+      let(:image)     { described_class.new(double(info: info)) }
+      let(:info)      { { "ParentId" => parent_id } }
+      let(:parent_id) { "12a324d4afc11e971cb86467681c0c94ff5bc0e946055ff92526bebee9477216" }
+
+      it "should return shortened parent_id" do
+        expect(image.short_parent_id).to eq parent_id[0..11]
+      end
+    end
+
+    describe "#tagged?" do
+      let(:image) { described_class.new(double(info: info)) }
+
+      context "when image is tagged" do
+        let(:info) { { "RepoTags" => ["hoge/fuga:latest"] } }
+
+        it "should return true" do
+          expect(image.tagged?).to be true
+        end
+      end
+
+      context "when image is not tagged" do
+        let(:info) { { "RepoTags" => ["<none>:<none>"] } }
+
+        it "should return false" do
+          expect(image.tagged?).to be false
+        end
+      end
+    end
+
     describe "#node_description" do
       let(:image) { described_class.new(double(info: info)) }
 
-      context "when container is tagged" do
+      context "when image is tagged" do
         let(:info) do
           {"id" => "3c", "ParentId" => "2b", "RepoTags" => ["dtan4/fuga:latest"]}
         end
 
-        it "should return diagram description" do
+        it "should return node description" do
           expect(image.node_description).to eq <<-EXPECT.strip
 image_3c[color=lawngreen, label="dtan4/fuga:latest", shape=box];
         EXPECT
         end
       end
 
-      context "when container is untagged" do
+      context "when image is untagged" do
         let(:info) do
           {"id" => "3c", "ParentId" => "2b", "RepoTags" => ["<none>:<none>"]}
         end
 
-        it "should return diagram description" do
+        it "should return node description" do
           expect(image.node_description).to eq <<-EXPECT.strip
 image_3c[color=lightgray, label="3c", shape=ellipse];
         EXPECT
